faebb26078d1d7695b404acb7993fe09b3727a33
Karol Grondzak <karol.grondzak@specure.com>
2018-11-17 15:22:58 +0100

    Chef and Difficult Contests solution added.

---
+++ CodeChef/src/codechef/Chftired.java
@@ -0,0 +1,19 @@
+package codechefprojects;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+    https://www.codechef.com/problems/CHFTIRED
+*/
+public class Chftired {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer testCases = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < testCases; ++tc) {
+            String data  = br.readLine();
+            System.out.println("YES");
+        }
+    }    
+}
